34cd9a82d4de3f86d94d0f3f91815110d63260d1
Thiago Costa de Paiva <tcpaiva@cern.ch>
2020-02-22 15:13:01 -0500

    Also regenerate output files when .py changes

--- tools/run.sh
+++ tools/run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-AUX=$(git diff-tree --no-commit-id --name-only -r "$CI_COMMIT_SHA" -- l0mdt_buses.csv) 
+AUX1=$(git diff-tree --no-commit-id --name-only -r "$CI_COMMIT_SHA" -- l0mdt_dataFormat.py) 
+AUX2=$(git diff-tree --no-commit-id --name-only -r "$CI_COMMIT_SHA" -- l0mdt_buses.csv)
 SHA="$(git rev-parse HEAD)"
 
-if [[ ( -n "$AUX" ) && ( "$SHA" == "$CI_COMMIT_SHA" ) ]]; then
+if [[ ( ( -n "$AUX1" ) || ( -n "$AUX2" ) ) && ( "$SHA" == "$CI_COMMIT_SHA" ) ]]; then
     python l0mdt_dataFormat.py -i l0mdt_buses.csv -r "$CI_COMMIT_SHA"
 fi
+
+
+
 exit 0
 
 

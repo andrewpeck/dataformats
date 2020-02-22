72c8eb3da6b64f4de3cd75dfb751a1699070704c
Thiago Costa De Paiva <thiago.paiva@cern.ch>
2020-02-22 14:59:51 +0000

    New pipeline approach

--- tools/run.sh
+++ tools/run.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+AUX=$(git diff-tree --no-commit-id --name-only -r "$CI_COMMIT_SHA" -- l0mdt_buses.csv) 
+SHA="$(git rev-parse HEAD)"
+
+if [[ ( -n "$AUX" ) && ( "$SHA" == "$CI_COMMIT_SHA" ) ]]; then
+    python l0mdt_dataFormat.py -i l0mdt_buses.csv -r "$CI_COMMIT_SHA"
+fi
+exit 0
+
+
 SHA="$(git rev-parse HEAD)"
 echo "CI SHA: $CI_COMMIT_SHA"
 echo "Job SHA: $SHA"

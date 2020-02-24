7d11e9743d0033a6786e61b172cec52249da9e37
Thiago Costa De Paiva <thiago.paiva@cern.ch>
2020-02-24 23:57:56 +0000

    Output in repo

---
+++ l0mdt_buses_constants.h
@@ -0,0 +1,395 @@
+// -------------------------------------------------------------------------------------------------
+// Auto-generated from:
+// https://docs.google.com/spreadsheets/d/1oJh-NPv990n6AzXXZ7cBaySrltqBO-eGucrsnOx_r4s
+// -------------------------------------------------------------------------------------------------
+
+#ifndef LOMDT_BUSES_CONSTANTS_H
+#define LOMDT_BUSES_CONSTANTS_H
+
+const char df_hash[] = "4ce208b8";
+
+// -------------------------------------------------------------------
+const int SLC_MUID_width = 20;
+
+// SLc Identification (up to 3)
+const int SLC_MUID_slcid_width = 2;
+const int SLC_MUID_slcid_lsb = 18;
+const int SLC_MUID_slcid_decb = 0;
+
+// SL board ID (based on the fiber ID)
+const int SLC_MUID_slid_width = 6;
+const int SLC_MUID_slid_lsb = 12;
+const int SLC_MUID_slid_decb = 0;
+
+// BCID from SLC
+const int SLC_MUID_bcid_width = 12;
+const int SLC_MUID_bcid_lsb = 0;
+const int SLC_MUID_bcid_decb = 0;
+
+// -------------------------------------------------------------------
+const int SLC_COMMON_width = 30;
+
+// SLc Identification (up to 3)
+const int SLC_COMMON_slcid_width = 2;
+const int SLC_COMMON_slcid_lsb = 28;
+const int SLC_COMMON_slcid_decb = 0;
+
+// SLc Eta Position
+const int SLC_COMMON_poseta_width = 14;
+const int SLC_COMMON_poseta_lsb = 14;
+const int SLC_COMMON_poseta_decb = 11;
+
+// SLc Phi Position
+const int SLC_COMMON_posphi_width = 9;
+const int SLC_COMMON_posphi_lsb = 5;
+const int SLC_COMMON_posphi_decb = 0;
+
+// SLc highest pT threshold passed
+const int SLC_COMMON_ptthresh_width = 4;
+const int SLC_COMMON_ptthresh_lsb = 1;
+const int SLC_COMMON_ptthresh_decb = 0;
+
+// SLc charge
+const int SLC_COMMON_charge_width = 1;
+const int SLC_COMMON_charge_lsb = 0;
+const int SLC_COMMON_charge_decb = 0;
+
+// -------------------------------------------------------------------
+const int SLC_ENDCAP_width = 68;
+
+// SLc Segment Angle wrt Eta position
+const int SLC_ENDCAP_seg_angdtheta_width = 7;
+const int SLC_ENDCAP_seg_angdtheta_lsb = 31;
+const int SLC_ENDCAP_seg_angdtheta_decb = 0;
+
+// SLc Segment Angle wrt Phi position
+const int SLC_ENDCAP_seg_angdphi_width = 4;
+const int SLC_ENDCAP_seg_angdphi_lsb = 27;
+const int SLC_ENDCAP_seg_angdphi_decb = 0;
+
+// NSW segment eta position
+const int SLC_ENDCAP_nswseg_poseta_width = 14;
+const int SLC_ENDCAP_nswseg_poseta_lsb = 13;
+const int SLC_ENDCAP_nswseg_poseta_decb = 13;
+
+// NSW segment phi position
+const int SLC_ENDCAP_nswseg_posphi_width = 8;
+const int SLC_ENDCAP_nswseg_posphi_lsb = 5;
+const int SLC_ENDCAP_nswseg_posphi_decb = 0;
+
+// NSW segment angle wrt Eta position
+const int SLC_ENDCAP_nswseg_angdtheta_width = 5;
+const int SLC_ENDCAP_nswseg_angdtheta_lsb = 0;
+const int SLC_ENDCAP_nswseg_angdtheta_decb = 0;
+
+// -------------------------------------------------------------------
+const int SLC_BARREL_width = 73;
+
+// SLc Hit Z Position in RPC0
+const int SLC_BARREL_rpc0_posz_width = 10;
+const int SLC_BARREL_rpc0_posz_lsb = 33;
+const int SLC_BARREL_rpc0_posz_decb = 0;
+
+// SLc Hit Z Position in RPC1
+const int SLC_BARREL_rpc1_posz_width = 10;
+const int SLC_BARREL_rpc1_posz_lsb = 23;
+const int SLC_BARREL_rpc1_posz_decb = 0;
+
+// SLc Hit Z Position in RPC2
+const int SLC_BARREL_rpc2_posz_width = 10;
+const int SLC_BARREL_rpc2_posz_lsb = 13;
+const int SLC_BARREL_rpc2_posz_decb = 0;
+
+// SLc Hit Z Position in RPC3
+const int SLC_BARREL_rpc3_posz_width = 10;
+const int SLC_BARREL_rpc3_posz_lsb = 3;
+const int SLC_BARREL_rpc3_posz_decb = 0;
+
+// SLc coincidence type
+const int SLC_BARREL_cointype_width = 3;
+const int SLC_BARREL_cointype_lsb = 0;
+const int SLC_BARREL_cointype_decb = 0;
+
+// -------------------------------------------------------------------
+const int SLCPROC_PIPELINE_COMMON_width = 35;
+
+// SLc busy flag
+const int SLCPROC_PIPELINE_COMMON_busy_width = 1;
+const int SLCPROC_PIPELINE_COMMON_busy_lsb = 34;
+const int SLCPROC_PIPELINE_COMMON_busy_decb = 0;
+
+// SLc board destination
+const int SLCPROC_PIPELINE_COMMON_destsl_width = 2;
+const int SLCPROC_PIPELINE_COMMON_destsl_lsb = 32;
+const int SLCPROC_PIPELINE_COMMON_destsl_decb = 0;
+
+// SLc phimod
+const int SLCPROC_PIPELINE_COMMON_phimod_width = 8;
+const int SLCPROC_PIPELINE_COMMON_phimod_lsb = 24;
+const int SLCPROC_PIPELINE_COMMON_phimod_decb = 0;
+
+// SLc Inner Vector MDT chamber ID
+const int SLCPROC_PIPELINE_COMMON_vec_mdtid_width = 6;
+const int SLCPROC_PIPELINE_COMMON_vec_mdtid_lsb = 18;
+const int SLCPROC_PIPELINE_COMMON_vec_mdtid_decb = 0;
+
+// SLc Middle Vector MDT chamber ID
+const int SLCPROC_PIPELINE_COMMON_vec_mdtid_width = 6;
+const int SLCPROC_PIPELINE_COMMON_vec_mdtid_lsb = 12;
+const int SLCPROC_PIPELINE_COMMON_vec_mdtid_decb = 0;
+
+// SLc Outer Vector MDT chamber ID
+const int SLCPROC_PIPELINE_COMMON_vec_mdtid_width = 6;
+const int SLCPROC_PIPELINE_COMMON_vec_mdtid_lsb = 6;
+const int SLCPROC_PIPELINE_COMMON_vec_mdtid_decb = 0;
+
+// SLc Extra Vector MDT chamber ID
+const int SLCPROC_PIPELINE_COMMON_vec_mdtid_width = 6;
+const int SLCPROC_PIPELINE_COMMON_vec_mdtid_lsb = 0;
+const int SLCPROC_PIPELINE_COMMON_vec_mdtid_decb = 0;
+
+// -------------------------------------------------------------------
+const int SLCPROC_PIPELINE_ENDCAP_width = 123;
+
+// -------------------------------------------------------------------
+const int SLCPROC_PIPELINE_BARREL_width = 128;
+
+// -------------------------------------------------------------------
+const int CSM_width = 34;
+
+// Channel number within TDC
+const int CSM_chanid_width = 7;
+const int CSM_chanid_lsb = 27;
+const int CSM_chanid_decb = 0;
+
+// Edge or pair mode
+const int CSM_edgemode_width = 2;
+const int CSM_edgemode_lsb = 25;
+const int CSM_edgemode_decb = 0;
+
+// TDC BCID
+const int CSM_coarsetime_width = 12;
+const int CSM_coarsetime_lsb = 13;
+const int CSM_coarsetime_decb = 0;
+
+// TDC fine time with BCID
+const int CSM_finetime_width = 5;
+const int CSM_finetime_lsb = 8;
+const int CSM_finetime_decb = 0;
+
+// Pulse width
+const int CSM_pulsewidth_width = 8;
+const int CSM_pulsewidth_lsb = 0;
+const int CSM_pulsewidth_decb = 0;
+
+// -------------------------------------------------------------------
+const int TDCFORMAT_width = 46;
+
+// Fiber ID within board
+const int TDCFORMAT_fiberid_width = 5;
+const int TDCFORMAT_fiberid_lsb = 7;
+const int TDCFORMAT_fiberid_decb = 0;
+
+// Elink ID within fiber
+const int TDCFORMAT_elinkid_width = 4;
+const int TDCFORMAT_elinkid_lsb = 3;
+const int TDCFORMAT_elinkid_decb = 0;
+
+// Valid bit
+const int TDCFORMAT_datavalid_width = 1;
+const int TDCFORMAT_datavalid_lsb = 2;
+const int TDCFORMAT_datavalid_decb = 0;
+
+// MDT Station Type (Inner, Middle, Outer, Extra)
+const int TDCFORMAT_stationid_width = 2;
+const int TDCFORMAT_stationid_lsb = 0;
+const int TDCFORMAT_stationid_decb = 0;
+
+// -------------------------------------------------------------------
+const int SLCPROC_HESF_width = 48;
+
+// SLc Muon Unique Identifier
+const int SLCPROC_HESF_SLC_MUID_width = 20;
+const int SLCPROC_HESF_SLC_MUID_lsb = 28;
+const int SLCPROC_HESF_SLC_MUID_decb = 0;
+
+// inner MDT segment chip destination
+const int SLCPROC_HESF_mdtseg_dest_width = 2;
+const int SLCPROC_HESF_mdtseg_dest_lsb = 26;
+const int SLCPROC_HESF_mdtseg_dest_decb = 0;
+
+// SLc Inner Vector MDT chamber ID
+const int SLCPROC_HESF_vec_mdtid_width = 6;
+const int SLCPROC_HESF_vec_mdtid_lsb = 20;
+const int SLCPROC_HESF_vec_mdtid_decb = 0;
+
+// SLc inner vector rho position
+const int SLCPROC_HESF_vec_pos_width = 10;
+const int SLCPROC_HESF_vec_pos_lsb = 10;
+const int SLCPROC_HESF_vec_pos_decb = 0;
+
+// SLc inner vector theta angle
+const int SLCPROC_HESF_vec_ang_width = 10;
+const int SLCPROC_HESF_vec_ang_lsb = 0;
+const int SLCPROC_HESF_vec_ang_decb = 0;
+
+// -------------------------------------------------------------------
+const int TUBEREMAP_width = 74;
+
+// Tube layer within one station
+const int TUBEREMAP_mdt_tube_layer_width = 6;
+const int TUBEREMAP_mdt_tube_layer_lsb = 68;
+const int TUBEREMAP_mdt_tube_layer_decb = 0;
+
+// Tube number within one station
+const int TUBEREMAP_mdt_tube_num_width = 10;
+const int TUBEREMAP_mdt_tube_num_lsb = 58;
+const int TUBEREMAP_mdt_tube_num_decb = 0;
+
+// Tube radial position
+const int TUBEREMAP_mdt_tube_rho_width = 18;
+const int TUBEREMAP_mdt_tube_rho_lsb = 40;
+const int TUBEREMAP_mdt_tube_rho_decb = 4;
+
+// Tube position along z
+const int TUBEREMAP_mdt_tube_z_width = 19;
+const int TUBEREMAP_mdt_tube_z_lsb = 21;
+const int TUBEREMAP_mdt_tube_z_decb = 4;
+
+// Tube (uncalibrated) time
+const int TUBEREMAP_mdt_tube_time_width = 21;
+const int TUBEREMAP_mdt_tube_time_lsb = 0;
+const int TUBEREMAP_mdt_tube_time_decb = 4;
+
+// -------------------------------------------------------------------
+const int HE_LSF_width = 39;
+
+// Hit Valid bit
+const int HE_LSF_mdt_valid_width = 1;
+const int HE_LSF_mdt_valid_lsb = 38;
+const int HE_LSF_mdt_valid_decb = 0;
+
+// Data Valid bit
+const int HE_LSF_data_valid_width = 1;
+const int HE_LSF_data_valid_lsb = 37;
+const int HE_LSF_data_valid_decb = 0;
+
+// Tube local position along precision coord
+const int HE_LSF_mdt_localx_width = 14;
+const int HE_LSF_mdt_localx_lsb = 0;
+const int HE_LSF_mdt_localx_decb = 4;
+
+// Tube local position along second coord
+const int HE_LSF_mdt_localy_width = 14;
+const int HE_LSF_mdt_localy_lsb = 14;
+const int HE_LSF_mdt_localy_decb = 4;
+
+// Tube drift radius
+const int HE_LSF_mdt_radius_width = 9;
+const int HE_LSF_mdt_radius_lsb = 28;
+const int HE_LSF_mdt_radius_decb = 5;
+
+// -------------------------------------------------------------------
+const int HE_CSF_width = 41;
+
+// Hit Valid bit
+const int HE_CSF_mdt_valid_width = 1;
+const int HE_CSF_mdt_valid_lsb = 40;
+const int HE_CSF_mdt_valid_decb = 0;
+
+// Data Valid bit
+const int HE_CSF_data_valid_width = 1;
+const int HE_CSF_data_valid_lsb = 39;
+const int HE_CSF_data_valid_decb = 0;
+
+// Tube local position along precision coord
+const int HE_CSF_mdt_localx_width = 15;
+const int HE_CSF_mdt_localx_lsb = 24;
+const int HE_CSF_mdt_localx_decb = 4;
+
+// Tube local position along second coord
+const int HE_CSF_mdt_localy_width = 15;
+const int HE_CSF_mdt_localy_lsb = 9;
+const int HE_CSF_mdt_localy_decb = 4;
+
+// Tube drift radius
+const int HE_CSF_mdt_radius_width = 9;
+const int HE_CSF_mdt_radius_lsb = 0;
+const int HE_CSF_mdt_radius_decb = 4;
+
+// -------------------------------------------------------------------
+const int SLCPIPELINE_PTCALC_width = 53;
+
+// -------------------------------------------------------------------
+const int SF_width = 55;
+
+// SF MDT segment valid bit
+const int SF_segvalid_width = 1;
+const int SF_segvalid_lsb = 28;
+const int SF_segvalid_decb = 0;
+
+// SF MDT segment position along the precision coord
+const int SF_segpos_width = 16;
+const int SF_segpos_lsb = 12;
+const int SF_segpos_decb = 2;
+
+// SF MDT segment angle along the precision coord
+const int SF_segangle_width = 11;
+const int SF_segangle_lsb = 1;
+const int SF_segangle_decb = 0;
+
+// SF MDT segment qualiry
+const int SF_segquality_width = 1;
+const int SF_segquality_lsb = 0;
+const int SF_segquality_decb = 0;
+
+// -------------------------------------------------------------------
+const int PTCALC_width = 50;
+
+// eta of the innermost MDT station segment position
+const int PTCALC_mtc_eta_width = 14;
+const int PTCALC_mtc_eta_lsb = 20;
+const int PTCALC_mtc_eta_decb = 11;
+
+// pT calculated by the pT Calc
+const int PTCALC_mtc_pt_width = 8;
+const int PTCALC_mtc_pt_lsb = 12;
+const int PTCALC_mtc_pt_decb = 1;
+
+// pT threshold satisfied by the MDT TC
+const int PTCALC_mtc_ptthresh_width = 4;
+const int PTCALC_mtc_ptthresh_lsb = 8;
+const int PTCALC_mtc_ptthresh_decb = 0;
+
+// charge determined from the pT calc
+const int PTCALC_mtc_charge_width = 1;
+const int PTCALC_mtc_charge_lsb = 7;
+const int PTCALC_mtc_charge_decb = 0;
+
+// # of segments used for calculating the pT
+const int PTCALC_mtc_nsegments_width = 2;
+const int PTCALC_mtc_nsegments_lsb = 1;
+const int PTCALC_mtc_nsegments_decb = 0;
+
+// quality of the MDT TC (TBC how this is defined)
+const int PTCALC_mtc_quality_width = 1;
+const int PTCALC_mtc_quality_lsb = 0;
+const int PTCALC_mtc_quality_decb = 0;
+
+// -------------------------------------------------------------------
+const int SLCPIPELINE_MTC_ENDCAP_width = 123;
+
+// -------------------------------------------------------------------
+const int SLCPIPELINE_MTC_BARREL_width = 128;
+
+// -------------------------------------------------------------------
+const int MTC_width = 64;
+
+// MDT processing flags
+const int MTC_mtc_procflags_width = 4;
+const int MTC_mtc_procflags_lsb = 3;
+const int MTC_mtc_procflags_decb = 0;
+
+// -------------------------------------------------------------------
+
+#endif // LOMDT_BUSES_CONSTANTS_H
---
+++ l0mdt_buses_types.svh
@@ -0,0 +1,263 @@
+// -------------------------------------------------------------------------------------------------
+// Auto-generated from:
+// https://docs.google.com/spreadsheets/d/1oJh-NPv990n6AzXXZ7cBaySrltqBO-eGucrsnOx_r4s
+// -------------------------------------------------------------------------------------------------
+
+#ifndef LOMDT_BUS_TYPES_H
+#define LOMDT_BUS_TYPES_H
+
+const char df_hash[] = "4ce208b8";
+
+// -------------------------------------------------------------------
+typedef struct SLC_MUID_n {
+    // SLc Identification (up to 3)
+    logic [1:0] slcid;
+    // SL board ID (based on the fiber ID)
+    logic [5:0] slid;
+    // BCID from SLC
+    logic [11:0] bcid;
+} SLC_MUID_rt;
+
+// -------------------------------------------------------------------
+typedef struct SLC_COMMON_n {
+    // SLc Identification (up to 3)
+    logic [1:0] slcid;
+    // SLc Eta Position
+    logic [13:0] poseta;
+    // SLc Phi Position
+    logic [8:0] posphi;
+    // SLc highest pT threshold passed
+    logic [3:0] ptthresh;
+    // SLc charge
+    logic [0:0] charge;
+} SLC_COMMON_rt;
+
+// -------------------------------------------------------------------
+typedef struct SLC_ENDCAP_n {
+    // struct SLC_COMMON
+    logic [29:0] SLC_COMMON;
+    // SLc Segment Angle wrt Eta position
+    logic [6:0] seg_angdtheta;
+    // SLc Segment Angle wrt Phi position
+    logic [3:0] seg_angdphi;
+    // NSW segment eta position
+    logic [13:0] nswseg_poseta;
+    // NSW segment phi position
+    logic [7:0] nswseg_posphi;
+    // NSW segment angle wrt Eta position
+    logic [4:0] nswseg_angdtheta;
+} SLC_ENDCAP_rt;
+
+// -------------------------------------------------------------------
+typedef struct SLC_BARREL_n {
+    // struct SLC_COMMON
+    logic [29:0] SLC_COMMON;
+    // SLc Hit Z Position in RPC0
+    logic [9:0] rpc0_posz;
+    // SLc Hit Z Position in RPC1
+    logic [9:0] rpc1_posz;
+    // SLc Hit Z Position in RPC2
+    logic [9:0] rpc2_posz;
+    // SLc Hit Z Position in RPC3
+    logic [9:0] rpc3_posz;
+    // SLc coincidence type
+    logic [2:0] cointype;
+} SLC_BARREL_rt;
+
+// -------------------------------------------------------------------
+typedef struct SLCPROC_PIPELINE_COMMON_n {
+    // SLc busy flag
+    logic [0:0] busy;
+    // SLc board destination
+    logic [1:0] destsl;
+    // SLc phimod
+    logic [7:0] phimod;
+    // SLc Inner Vector MDT chamber ID
+    logic [5:0] vec_mdtid;
+} SLCPROC_PIPELINE_COMMON_rt;
+
+// -------------------------------------------------------------------
+typedef struct SLCPROC_PIPELINE_ENDCAP_n {
+    // struct SLCPROC_PIPELINE_COMMON
+    logic [34:0] SLCPROC_PIPELINE_COMMON;
+    // struct SLC_ENDCAP
+    logic [67:0] SLC_ENDCAP;
+    // struct SLC_MUID
+    logic [19:0] SLC_MUID;
+} SLCPROC_PIPELINE_ENDCAP_rt;
+
+// -------------------------------------------------------------------
+typedef struct SLCPROC_PIPELINE_BARREL_n {
+    // struct SLCPROC_PIPELINE_COMMON
+    logic [34:0] SLCPROC_PIPELINE_COMMON;
+    // struct SLC_BARREL
+    logic [72:0] SLC_BARREL;
+    // struct SLC_MUID
+    logic [19:0] SLC_MUID;
+} SLCPROC_PIPELINE_BARREL_rt;
+
+// -------------------------------------------------------------------
+typedef struct CSM_n {
+    // Channel number within TDC
+    logic [6:0] chanid;
+    // Edge or pair mode
+    logic [1:0] edgemode;
+    // TDC BCID
+    logic [11:0] coarsetime;
+    // TDC fine time with BCID
+    logic [4:0] finetime;
+    // Pulse width
+    logic [7:0] pulsewidth;
+} CSM_rt;
+
+// -------------------------------------------------------------------
+typedef struct TDCFORMAT_n {
+    // struct CSM
+    logic [33:0] CSM;
+    // Fiber ID within board
+    logic [4:0] fiberid;
+    // Elink ID within fiber
+    logic [3:0] elinkid;
+    // Valid bit
+    logic [0:0] datavalid;
+    // MDT Station Type (Inner, Middle, Outer, Extra)
+    logic [1:0] stationid;
+} TDCFORMAT_rt;
+
+// -------------------------------------------------------------------
+typedef struct SLCPROC_HESF_n {
+    // struct SLC_MUID
+    logic [19:0] SLC_MUID;
+    // inner MDT segment chip destination
+    logic [1:0] mdtseg_dest;
+    // SLc Inner Vector MDT chamber ID
+    logic [5:0] vec_mdtid;
+    // SLc inner vector rho position
+    logic [9:0] vec_pos;
+    // SLc inner vector theta angle
+    logic [9:0] vec_ang;
+} SLCPROC_HESF_rt;
+
+// -------------------------------------------------------------------
+typedef struct TUBEREMAP_n {
+    // Tube layer within one station
+    logic [5:0] mdt_tube_layer;
+    // Tube number within one station
+    logic [9:0] mdt_tube_num;
+    // Tube radial position
+    logic [17:0] mdt_tube_rho;
+    // Tube position along z
+    logic [18:0] mdt_tube_z;
+    // Tube (uncalibrated) time
+    logic [20:0] mdt_tube_time;
+} TUBEREMAP_rt;
+
+// -------------------------------------------------------------------
+typedef struct HE_LSF_n {
+    // Hit Valid bit
+    logic [0:0] mdt_valid;
+    // Data Valid bit
+    logic [0:0] data_valid;
+    // Tube local position along precision coord
+    logic [13:0] mdt_localx;
+    // Tube local position along second coord
+    logic [13:0] mdt_localy;
+    // Tube drift radius
+    logic [8:0] mdt_radius;
+} HE_LSF_rt;
+
+// -------------------------------------------------------------------
+typedef struct HE_CSF_n {
+    // Hit Valid bit
+    logic [0:0] mdt_valid;
+    // Data Valid bit
+    logic [0:0] data_valid;
+    // Tube local position along precision coord
+    logic [14:0] mdt_localx;
+    // Tube local position along second coord
+    logic [14:0] mdt_localy;
+    // Tube drift radius
+    logic [8:0] mdt_radius;
+} HE_CSF_rt;
+
+// -------------------------------------------------------------------
+typedef struct SLCPIPELINE_PTCALC_n {
+    // struct SLC_MUID
+    logic [19:0] SLC_MUID;
+    // (COPY)
+    logic [5:0] vec_mdtid;
+    // (COPY)
+    logic [7:0] phimod;
+    // (COPY)
+    logic [0:0] charge;
+} SLCPIPELINE_PTCALC_rt;
+
+// -------------------------------------------------------------------
+typedef struct SF_n {
+    // struct SLC_MUID
+    logic [19:0] SLC_MUID;
+    // (COPY)
+    logic [5:0] vec_mdtid;
+    // SF MDT segment valid bit
+    logic [0:0] segvalid;
+    // SF MDT segment position along the precision coord
+    logic [15:0] segpos;
+    // SF MDT segment angle along the precision coord
+    logic [10:0] segangle;
+    // SF MDT segment qualiry
+    logic [0:0] segquality;
+} SF_rt;
+
+// -------------------------------------------------------------------
+typedef struct PTCALC_n {
+    // struct SLC_MUID
+    logic [19:0] SLC_MUID;
+    // eta of the innermost MDT station segment position
+    logic [13:0] mtc_eta;
+    // pT calculated by the pT Calc
+    logic [7:0] mtc_pt;
+    // pT threshold satisfied by the MDT TC
+    logic [3:0] mtc_ptthresh;
+    // charge determined from the pT calc
+    logic [0:0] mtc_charge;
+    // # of segments used for calculating the pT
+    logic [1:0] mtc_nsegments;
+    // quality of the MDT TC (TBC how this is defined)
+    logic [0:0] mtc_quality;
+} PTCALC_rt;
+
+// -------------------------------------------------------------------
+typedef struct SLCPIPELINE_MTC_ENDCAP_n {
+    // struct SLCPROC_PIPELINE_ENDCAP
+    logic [19:0] SLCPROC_PIPELINE_ENDCAP;
+} SLCPIPELINE_MTC_ENDCAP_rt;
+
+// -------------------------------------------------------------------
+typedef struct SLCPIPELINE_MTC_BARREL_n {
+    // struct SLCPROC_PIPELINE_BARREL
+    logic [13:0] SLCPROC_PIPELINE_BARREL;
+} SLCPIPELINE_MTC_BARREL_rt;
+
+// -------------------------------------------------------------------
+typedef struct MTC_n {
+    // struct SLC_COMMON
+    logic [29:0] SLC_COMMON;
+    // (COPY)
+    logic [13:0] mtc_eta;
+    // (COPY)
+    logic [7:0] mtc_pt;
+    // (COPY)
+    logic [3:0] mtc_ptthresh;
+    // (COPY)
+    logic [0:0] mtc_charge;
+    // MDT processing flags
+    logic [3:0] mtc_procflags;
+    // (COPY)
+    logic [1:0] mtc_nsegments;
+    // (COPY)
+    logic [0:0] mtc_quality;
+} MTC_rt;
+
+// -------------------------------------------------------------------
+
+#endif // LOMDT_BUS_TYPES_H
